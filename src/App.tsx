3b81cbb75d7883066ebda1c94f9a65460950480c
Randy Cullins <randycullins2018@gmail.com>
2024-12-21 10:26:48 -0700

    created hook for the handle functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,11 @@
-import { useState } from "react";
-import { DndContext, DragEndEvent } from "@dnd-kit/core";
-import { v4 } from "uuid";
+import { DndContext } from "@dnd-kit/core";
 
 import TaskForm from "./components/TaskForm";
 import Column from "./components/Column";
-
-import { INITIAL_TASKS } from "./data";
-import type { Task } from "./types";
+import { useHandleFunctions } from "./hooks/useHandleFunctions";
 
 export default function App() {
-  const [tasks, setTasks] = useState<Task[]>(INITIAL_TASKS);
-
-  function handleAddTask(title: string, description: string) {
-    const newId = v4();
-
-    const newTask: Task = {
-      id: newId,
-      status: "TODO",
-      title,
-      description,
-    };
-
-    setTasks((prev) => [...prev, newTask]);
-  }
-
-  function handleDragEnd(event: DragEndEvent) {
-    const { active, over } = event;
-
-    if (!over) return;
-
-    const taskId = active.id as string;
-    const newStatus = over.id as Task["status"];
-
-    setTasks((prev) =>
-      prev.map((task) =>
-        task.id === taskId
-          ? {
-              ...task,
-              status: newStatus,
-            }
-          : task
-      )
-    );
-  }
+  const { tasks, handleAddTask, handleDragEnd } = useHandleFunctions();
 
   return (
     <div className="min-h-screen flex flex-col justify-start items-center bg-grayscale-900 p-4 text-white">
---
+++ src/hooks/useHandleFunctions.ts
@@ -0,0 +1,50 @@
+import { DragEndEvent } from "@dnd-kit/core";
+import { Task } from "../types";
+import { useState } from "react";
+import { v4 } from "uuid";
+import { INITIAL_TASKS } from "../data";
+
+interface UseHandleFunctions {
+  tasks: Task[];
+  handleAddTask: (title: string, description: string) => void;
+  handleDragEnd: (event: DragEndEvent) => void;
+}
+
+export const useHandleFunctions = (): UseHandleFunctions => {
+  const [tasks, setTasks] = useState<Task[]>(INITIAL_TASKS);
+
+  function handleAddTask(title: string, description: string) {
+    const newId = v4();
+
+    const newTask: Task = {
+      id: newId,
+      status: "TODO",
+      title,
+      description,
+    };
+
+    setTasks((prev) => [...prev, newTask]);
+  }
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+
+    if (!over) return;
+
+    const taskId = active.id as string;
+    const newStatus = over.id as Task["status"];
+
+    setTasks((prev) =>
+      prev.map((task) =>
+        task.id === taskId
+          ? {
+              ...task,
+              status: newStatus,
+            }
+          : task
+      )
+    );
+  }
+
+  return { tasks, handleAddTask, handleDragEnd };
+};
